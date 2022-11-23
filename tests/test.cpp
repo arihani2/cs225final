4d183db7fdd4dd8806dff889f7474a5ef72504a4
eduardomartinez223 <em18@illinois.edu>
2022-11-23 22:20:21 +0000

    commit

--- tests/test.cpp
+++ tests/test.cpp
@@ -1,6 +1,13 @@
 #include <catch2/catch_test_macros.hpp>
+
 #include "graph.h"
 #include "edge.h"
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
 
 TEST_CASE( "test", "[test]") {
     REQUIRE(1 == 1);
