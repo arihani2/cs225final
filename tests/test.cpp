1473e0e4d33c11b7d4ad995c4eede9fae63ead19
eduardomartinez223 <em18@illinois.edu>
2022-11-28 18:08:10 +0000

    Created test cases for graph class

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,13 +4,88 @@
 #include "edge.h"
 #include "../src/graph.h"
 #include "../src/edge.h"
-
 #include <iostream>
 #include <vector>
 #include <string>
 
 using namespace std;
 
-TEST_CASE( "test", "[test]") {
-    REQUIRE(1 == 1);
+Graph graph("../tests/airport.txt", "../tests/route.txt");
+
+TEST_CASE("vertexExists1", "[weight=1]" "[vertexExists]") {
+    REQUIRE(graph.vertexExists("PPG"));
+}
+
+TEST_CASE("vertexExists2", "[weight=1]" "[vertexExists]") {
+    REQUIRE(graph.vertexExists("THR"));
+}
+
+TEST_CASE("vertexExists3", "[weight=1]" "[vertexExists]") {
+    REQUIRE(graph.vertexExists("BYC"));
+}
+
+TEST_CASE("vertexExists4", "[weight=1]" "[vertexExists]") {
+    REQUIRE(graph.vertexExists("LUH"));
+}
+
+TEST_CASE("vertexExists5", "[weight=1]" "[vertexExists]") {
+    REQUIRE(graph.vertexExists("CDC"));
+}
+
+TEST_CASE("getEdgeWeight1", "[weight=1]" "[getEdgeWeight]") {
+    REQUIRE(graph.getEdgeWeight("ORD", "DEC") == 251);
+}
+
+TEST_CASE("getEdgeWeight2", "[weight=1]" "[getEdgeWeight]") {
+    REQUIRE(graph.getEdgeWeight("VCE", "CGN") == 709);
+}
+
+TEST_CASE("getEdgeWeight3", "[weight=1]" "[getEdgeWeight]") {
+    REQUIRE(graph.getEdgeWeight("VCE", "ORD") == -1);
+}
+
+TEST_CASE("edgeExists1" , "[weight=1]" "[edgeExists]") {
+    REQUIRE(graph.edgeExists("ORD", "JFK"));
+}
+
+TEST_CASE("edgeExists2" , "[weight=1]" "[edgeExists]") {
+    REQUIRE(graph.edgeExists("ATL", "CUN"));
+}
+
+TEST_CASE("edgeExists3" , "[weight=1]" "[edgeExists]") {
+    REQUIRE(!(graph.edgeExists("IKA", "ORD")));
+}
+
+//Insert Vertex
+TEST_CASE("insertVertex1", "[weight=1]" "[insertVertex]") {
+    REQUIRE(!(graph.vertexExists("LOL")));
+    graph.insertVertex("LOL");
+    REQUIRE(graph.vertexExists("LOL"));
+}
+
+TEST_CASE("insertVertex2", "[weight=1]" "[insertVertex]") {
+    graph.insertVertex("ORD");
+    REQUIRE(graph.vertexExists("ORD"));
+}
+
+//Insert Edge
+TEST_CASE("insertEdge1", "[weight=1]" "[insertEdge]") {
+    REQUIRE(graph.insertEdge("LOL", "POO", 100));
+}
+
+TEST_CASE("insertEdge2", "[weight=1]" "[insertEdge]") {
+    int weight = graph.getEdgeWeight("ORD", "ANC");
+    REQUIRE(!(graph.insertEdge("ORD", "ANC", weight)));
+}
+
+//getAdjacent
+TEST_CASE("getAdjacent1", "[weight=1]" "[getAdjacent]") {
+    vector<Vertex> adj = graph.getAdjacent("ORD");
+    for (unsigned i = 0; i < adj.size(); i++) {
+        if (graph.getEdgeWeight("ORD", adj[i]) == -1) {
+            REQUIRE(graph.getEdgeWeight(adj[i], "ORD") != -1); 
+        } else {
+            REQUIRE(graph.getEdgeWeight("ORD", adj[i]) != -1);
+        }
+    }
 }
